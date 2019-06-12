5db25ecc24eecac95ec2a70fdda6198f12079fe8
maheshinder19 <mahesh.inder85@gmail.com>
2019-06-12 15:42:58 +0530

    passport setup

---
+++ server/config/keys.js
@@ -0,0 +1,18 @@
+// add this file to .gitignore
+// add this file to .gitignore
+
+module.exports = {
+  google: {
+    clientID:
+      "915001291508-74s995lv0gueq1bt7j38h9bagao1v3lu.apps.googleusercontent.com",
+    clientSecret: "AhJJD7575JStU1R9me-gpbOv"
+  },
+  mongodb: {
+    dbURI:
+      // "mongodb+srv://mi:mi@cluster0-7e6pg.mongodb.net/test?retryWrites=true&w=majority"
+      "mongodb://localhost:27017/ttnBuzz"
+  },
+  session: {
+    cookieKey: "bingo"
+  }
+};
